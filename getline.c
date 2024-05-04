928421b9ad54f5bf01308cf587e3129223a6d603
root <jamesalfreddie@gmail.com>
2024-05-04 12:44:53 +0000

    New files have been added

---
+++ getline.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+char *read_line(void)
+{
+char *line = NULL;
+size_t bufsize = 0;
+if (getline(&line,&bufsize,stdin) == -1)
+{
+	if (feod(stdin))
+	{
+	free(line);
+	exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		free(line);
+		perror("Error from stdin");
+		exit(EXIT_FAILURE);
+	}
+}
+return (line);
+
+}
