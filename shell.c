747c153609e8c7bd4f29c84c3758c790e287851f
jamesbondie <jamesalfreddie@gmail.com>
2024-05-07 10:45:02 +0400

    declaration errors removed

--- shell.c
+++ shell.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-int main (int ac, char **av)
+int main ()
 
 {
         char *buffer;
         size_t bufsize = 5;
         pid_t my_pid;
         int blabla;
+	char *args[] = { "mini_shell", NULL, NULL};
         buffer = malloc(bufsize*sizeof(char));
         while (1)
         {
@@ -17,12 +18,11 @@ int main (int ac, char **av)
                 if (my_pid == 0)
                 {
                         printf("#cisfun$ ");
-                        getline(&buffer,&bufsize,stdin)
+                        getline(&buffer,&bufsize,stdin);
                         if (buffer[strlen(buffer) - 1] == '\n')
                                 buffer[strlen(buffer) - 1] = '\0';
-                        char* const newenvironment[] = {NULL};
-                        char *args[] = { "mini_shell", buffer, NULL };
-                        execve(buffer, av, newenvironment);
+                        args[2] = buffer;
+                        execve(buffer, args, NULL);
                 }
         }
 }
