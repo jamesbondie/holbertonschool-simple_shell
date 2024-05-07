eee091368abcfd678df09e56e44fefc8f69c6538
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-07 07:59:06 +0000

    isatty checked

--- shell.c
+++ shell.c
@@ -1,28 +1,42 @@
 #include "main.h"
-
-int main ()
-
+int main (int ac, char **av)
 {
         char *buffer;
         size_t bufsize = 5;
         pid_t my_pid;
         int blabla;
-	char *args[] = { "mini_shell", NULL, NULL};
+        char *args[] = { "mini_shell", NULL, NULL};
         buffer = malloc(bufsize*sizeof(char));
-        while (1)
+        if(isatty(STDIN_FILENO) && ac > 0)
         {
-                my_pid = fork();
-
-                if (my_pid != 0)
-                        wait(&blabla);
-                if (my_pid == 0)
+                while (1)
                 {
                         printf("#cisfun$ ");
-                        getline(&buffer,&bufsize,stdin);
+                        if (getline(&buffer,&bufsize,stdin) == -1)
+                                free(buffer);
                         if (buffer[strlen(buffer) - 1] == '\n')
                                 buffer[strlen(buffer) - 1] = '\0';
                         args[2] = buffer;
-                        execve(buffer, args, NULL);
+                        my_pid = fork();
+                        if (my_pid != 0)
+                                wait(&blabla);
+                        if (my_pid == 0)
+                        {
+                                if (execve(buffer, args, NULL) == -1)
+                                        perror(av[0]);
+                        }
                 }
         }
+        else
+        {
+                if (getline(&buffer,&bufsize,stdin) == -1)
+                        free(buffer);
+                if (buffer[strlen(buffer) - 1] == '\n')
+                        buffer[strlen(buffer) - 1] = '\0';
+                args[2] = buffer;
+                if (execve(buffer, args, NULL) == -1)
+                        perror(av[0]);
+        }
+        return 0;
 }
+
