beb6b092bf7308ceb6cca2c84d9d947416be511f
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-08 19:43:14 +0000

    step

--- shell.c
+++ shell.c
@@ -3,11 +3,29 @@ extern char** environ;
 int main (int ac, char **av)
 {
         size_t bufsize = 5;
+        pid_t my_pid;
+        int blabla;
         char *buffer = malloc(sizeof(char) * bufsize);
         char *args[65];
         if(isatty(0) && ac > 0)
         {
-                fprintf(stderr, "adsf");
+                while(1)
+                {
+                        printf("#cisfun$ ");
+                        if (getline(&buffer,&bufsize,stdin) == -1)
+                                free(buffer);
+                        if (buffer[strlen(buffer) - 1] == '\n')
+                                buffer[strlen(buffer) - 1] = '\0';
+                        my_pid = fork();
+                        if (my_pid != 0)
+                                wait(&blabla);
+                        if (my_pid == 0)
+                        {
+                                if (execve(buffer, args, NULL) == -1)
+                                        perror(av[0]);
+                        } 
+                }
+
         }
         else
         {
@@ -15,9 +33,8 @@ int main (int ac, char **av)
                         free(buffer);
                 if (buffer[strlen(buffer) - 1] == '\n')
                         buffer[strlen(buffer) - 1] = '\0';
-                
                 if (execve(buffer, args, environ) == -1)
                         perror(av[0]);
         }
-                return 0;
+                exit(EXIT_SUCCESS);
 }
