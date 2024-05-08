61d6592918ba5bf2c84a8184a06283cb14e33527
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-08 18:20:11 +0000

    changed

--- shell.c
+++ shell.c
@@ -2,39 +2,17 @@
 extern char** environ;
 int main (int ac, char **av)
 {
-        char *buffer;
-        size_t bufsize = 5;
-        pid_t my_pid;
-        int blabla;
-        char *args[] = { "bin", "-c", NULL};
-        buffer = malloc(bufsize*sizeof(char));
+        char *args[] = { NULL, NULL, NULL};
         if(isatty(0) && ac > 0)
         {
-                while (1)
-                {
-                        printf("#cisfun$ ");
-                        if (getline(&buffer,&bufsize,stdin) == -1)
-                                free(buffer);
-                        if (buffer[strlen(buffer) - 1] == '\n')
-                                buffer[strlen(buffer) - 1] = '\0';
-                        my_pid = fork();
-                        if (my_pid != 0)
-                                wait(&blabla);
-                        if (my_pid == 0)
-                        {
-                                if (execve(buffer, args, NULL) == -1)
-                                        perror(av[0]);
-                        }
-                }
+                fprintf(stderr, "adsf");
         }
         else
         {
-                if (getline(&buffer,&bufsize,stdin) == -1)
-                        free(buffer);
-                if (buffer[strlen(buffer) - 1] == '\n')
-                        buffer[strlen(buffer) - 1] = '\0';
-                if (execve(buffer, args, environ) == -1)
+                args[1] = "-c";
+                args[0] = "/bin/ls";
+                if (execve(args[0], args, environ) == -1)
                         perror(av[0]);
-        }
-        return 0;
+                }
+                return 0;
 }
