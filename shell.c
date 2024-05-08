1397c22b83901ab219773f58167b0cc76ba0d4b5
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-08 19:31:07 +0000

    step

--- shell.c
+++ shell.c
@@ -15,9 +15,9 @@ int main (int ac, char **av)
                         free(buffer);
                 if (buffer[strlen(buffer) - 1] == '\n')
                         buffer[strlen(buffer) - 1] = '\0';
-                args[0] = buffer;
+                args[0] = "/bin/ls";
                 if (execve(args[0], args, environ) == -1)
                         perror(av[0]);
-                }
+        }
                 return 0;
 }
