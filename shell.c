b551bf9d9c1fdc4f3485949bd050ca1c28dc1fd5
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-08 18:54:04 +0000

    changed

--- shell.c
+++ shell.c
@@ -2,13 +2,14 @@
 extern char** environ;
 int main (int ac, char **av)
 {
-        char *args[] = { "/bin/ls", "-s", NULL};
+        char *args[65];
         if(isatty(0) && ac > 0)
         {
                 fprintf(stderr, "adsf");
         }
         else
         {
+                args[0] = "/bin/ls";
                 if (execve(args[0], args, environ) == -1)
                         perror(av[0]);
                 }
