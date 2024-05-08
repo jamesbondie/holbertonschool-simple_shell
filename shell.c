1e8243797c633f8c8b4fd291c0dcd284952154dc
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-08 16:33:55 +0000

    try again

--- shell.c
+++ shell.c
@@ -1,42 +1,16 @@
 #include "main.h"
+extern char** environ;
 int main (int ac, char **av)
 {
-        char *buffer;
-        size_t bufsize = 5;
-        pid_t my_pid;
-        int blabla;
-        char *args[] = { "mini_shell", NULL, NULL};
-        buffer = malloc(bufsize*sizeof(char));
-        if(isatty(STDIN_FILENO) && ac > 0)
+        char *args[] = {"ls", NULL};
+        if(isatty(0) && ac > 0)
         {
-                while (1)
-                {
-                        printf("#cisfun$ ");
-                        if (getline(&buffer,&bufsize,stdin) == -1)
-                                free(buffer);
-                        if (buffer[strlen(buffer) - 1] == '\n')
-                                buffer[strlen(buffer) - 1] = '\0';
-                        args[2] = buffer;
-                        my_pid = fork();
-                        if (my_pid != 0)
-                                wait(&blabla);
-                        if (my_pid == 0)
-                        {
-                                if (execve(buffer, args, NULL) == -1)
-                                        perror(av[0]);
-                        }
-                }
+                printf("asdf");
         }
         else
         {
-                if (getline(&buffer,&bufsize,stdin) == -1)
-                        free(buffer);
-                if (buffer[strlen(buffer) - 1] == '\n')
-                        buffer[strlen(buffer) - 1] = '\0';
-                args[2] = buffer;
-                if (execve(buffer, args, NULL) == -1)
+                if(execve("/bin/ls", args, environ) == -1)
                         perror(av[0]);
         }
         return 0;
 }
-
