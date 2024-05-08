04b8672b6f677cef83d13bffdec808ae29b516f1
jamesbondie <jamesalfreddie@gmail.com>
2024-05-08 12:23:23 +0400

    execve checked with isatty

--- shell.c
+++ shell.c
@@ -3,17 +3,22 @@
 
 int main(void)
 {
-    char *argv[] = { "/bin/sh", "-c", "env", 0 };
-    char *envp[] =
-    {
-        "HOME=/",
-        "PATH=/bin:/usr/bin",
-        "TZ=UTC0",
-        "USER=beelzebub",
-        "LOGNAME=tarzan",
-        0
-    };
-    execve(argv[0], &argv[0], envp);
-    fprintf(stderr, "Oops!\n");
-    return -1;
+    if (isatty(STDIN_FILENO)) {
+        char *argv[] = {"/bin/sh", "-c", "env", 0};
+        char *envp[] = {
+            "HOME=/",
+            "PATH=/bin:/usr/bin",
+            "TZ=UTC0",
+            "USER=beelzebub",
+            "LOGNAME=tarzan",
+            0};
+
+        execve(argv[0], &argv[0], envp);
+        fprintf(stderr, "Oops!\n");
+        return -1;
+    } else {
+        fprintf(stderr, "Not connected to a terminal!\n");
+        return -1;
+    }
 }
+
