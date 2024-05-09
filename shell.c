763eef0857663eb96880589eeab217677627c37c
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-09 08:58:37 +0000

    checking

--- shell.c
+++ shell.c
@@ -2,6 +2,8 @@
 extern char** environ;
 int main (int ac, char **av)
 {
+        pid_t my_pid;
+        int status;
         char *args[65];
         if(isatty(0) && ac > 0)
         {
@@ -9,10 +11,23 @@ int main (int ac, char **av)
         }
         else
         {
-                if (execve("/bin/ls", args, environ) == -1)
+                my_pid = fork();
+                if (my_pid == -1)
                 {
-                        fprintf(stderr, "%s: 1: not found\n", av[0]);
-                        exit(90);
+                        perror("fork");
+                        exit(EXIT_FAILURE);
+                }
+                else if (my_pid == 0)
+                {
+                        if (execve("/bin/ls", args, environ) == -1)
+                        {
+                                fprintf(stderr, "%s: 1: not found\n", av[0]);
+                                exit(90);
+                        }
+                }
+                else 
+                {
+                        wait(&status);
                 }
         }
         return(60);
