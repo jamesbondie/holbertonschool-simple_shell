aecb6ff7107cf0d7d4ad158c9ed9bb25a863676b
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-09 11:26:16 +0000

    ez win

--- shell.c
+++ shell.c
@@ -4,10 +4,11 @@ extern char** environ;
 int main (int ac, char **av)
 {
     pid_t my_pid;
+    size_t bufsize = 3;
     int status;
     char *args[2];
-
-    args[0] = "/bin/ls";
+    char *buffer = malloc(bufsize * sizeof(char));
+    args[0] = NULL;
     args[1] = NULL;
 
     if(isatty(0) && ac > 0)
@@ -16,25 +17,33 @@ int main (int ac, char **av)
     }
     else
     {
-        my_pid = fork();
-        if (my_pid == -1)
+        while (getline(&buffer, &bufsize, stdin) != -1)
         {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        else if (my_pid == 0)
-        {
-            if (execve(args[0], args, environ) == -1)
+            if (buffer[strlen(buffer) - 1] == '\n')
+                buffer[strlen(buffer) - 1] = '\0';
+
+            args[0] = buffer;
+            my_pid = fork();
+            if (my_pid == -1)
             {
-                fprintf(stderr, "%s: 1: not found\n", av[0]);
+                perror("fork");
                 exit(EXIT_FAILURE);
             }
-        }
-        else 
-        {
-            wait(&status);
-            
+            else if (my_pid == 0)
+            {
+                if (execve(args[0], args, environ) == -1)
+                {
+                    fprintf(stderr, "%s: 1: %s: not found\n", av[0], buffer);
+                    exit(EXIT_FAILURE);
+                }
+            }
+                
+            else 
+            {
+                wait(&status); 
+            }
         }
     }
+    
     return 0;
 }
