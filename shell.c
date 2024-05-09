d7a84755dd79b4353fc7f2055696ce8666ba685e
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-09 09:36:00 +0000

    test

--- shell.c
+++ shell.c
@@ -1,34 +1,43 @@
 #include "main.h"
 extern char** environ;
+
 int main (int ac, char **av)
 {
-        pid_t my_pid;
-        int status;
-        char *args[65];
-        if(isatty(0) && ac > 0)
+    pid_t my_pid;
+    int status;
+    char *args[2];
+
+    args[0] = "/bin/ls";
+    args[1] = NULL;
+
+    if(isatty(0) && ac > 0)
+    {
+            printf("asdfsdf");
+    }
+    else
+    {
+        my_pid = fork();
+        if (my_pid == -1)
         {
-                printf("asdfsdf");
+            perror("fork");
+            exit(EXIT_FAILURE);
         }
-        else
+        else if (my_pid == 0)
         {
-                my_pid = fork();
-                if (my_pid == -1)
-                {
-                        perror("fork");
-                        exit(EXIT_FAILURE);
-                }
-                else if (my_pid == 0)
-                {
-                        if (execve("/bin/ls", args, environ) == -1)
-                        {
-                                fprintf(stderr, "%s: 1: not found\n", av[0]);
-                                exit(90);
-                        }
-                }
-                else 
-                {
-                        wait(&status);
-                }
+            if (execve(args[0], args, environ) == -1)
+            {
+                fprintf(stderr, "%s: 1: not found\n", av[0]);
+                exit(EXIT_FAILURE);
+            }
         }
-        return(60);
+        else 
+        {
+            wait(&status);
+            if (WIFEXITED(status))
+            {
+                return WEXITSTATUS(status);
+            }
+        }
+    }
+    return 0;
 }
