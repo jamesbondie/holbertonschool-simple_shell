3884d49c1f786add80ecb5ad13f341703d2eeef5
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-10 12:03:07 +0000

    exit added, interactive mode used, env handled

--- shell.c
+++ shell.c
@@ -26,10 +26,10 @@ void _getenv(const char* name, char *args[64])
     }
 }
 
-void args_yazan(char *arv[64], char *kod_tutan)
+void args_writer(char *arv[64], char *code_holder)
 {
     char *args[64];
-    char* nese = strdup(kod_tutan);
+    char *nese = strdup(code_holder);
     int i = 0, j = 0;
         _getenv("PATH", args);
     while (args[i])
@@ -49,7 +49,7 @@ void args_yazan(char *arv[64], char *kod_tutan)
 int main(int ac, char **av)
 {
         pid_t my_pid;
-        size_t bufsize = 3;
+        size_t bufsize = 64;
         int status;
         char *args[64];
         char *buffer = malloc(bufsize * sizeof(char));
@@ -63,7 +63,73 @@ int main(int ac, char **av)
         args[0] = NULL;
         args[1] = NULL;
         if (isatty(0) && ac > 0)
-                printf("asdfsdf");
+                {
+                        while (1)
+                        {       i = 0;
+                                printf("$ ");
+                                if (getline(&buffer, &bufsize, stdin) != -1)
+                                {
+                                        if (buffer[strlen(buffer) - 1] == '\n')
+                                                buffer[strlen(buffer) - 1] = '\0';
+                                        token = strtok(buffer, " \n");
+
+                                        if (token == NULL)
+                                        {
+                                                free(buffer);
+                                                exit(EXIT_SUCCESS);
+                                        }
+                                        while (token != NULL)
+                                        {
+                                                args[i] = strdup(token);
+                                                if (!args[i])
+                                                {
+                                                        perror("strdup");
+                                                        free(buffer);
+                                                        exit(EXIT_FAILURE);
+                                                }
+                                                token = strtok(NULL, " \n");
+                                                i++;
+
+                                        }
+                                        args[i] = NULL;
+
+                                        if (strcmp(args[0], "exit") == 0)
+                                                break;
+                                        my_pid = fork();
+
+                                        if (my_pid == -1)
+                                        {
+                                                perror("fork");
+                                                exit(EXIT_FAILURE);
+                                        }
+                                        else if (my_pid == 0)
+                                        {
+
+                                                if (strchr(args[0], '/') == 0)
+                                                {
+                                                        args_writer(args, args[0]);
+                                                }
+                                                if (execve(args[0], args, environ) == -1)
+                                                {
+                                                        fprintf(stderr, "%s: 1: %s: not found\n", av[0], buffer);
+                                                        free(buffer);
+                                                        for (j = 0; j < i; j++)
+                                                        free(args[j]);
+                                                        exit(EXIT_FAILURE);
+                                                }
+                                        }
+                                        else
+                                        {
+                                        wait(&status);
+                                        }
+                                        for (j = 0; j < i; j++)
+                                                free(args[j]);
+                                                                }
+                                else
+                                        break;
+                        }
+                }
+
         else
         {
                 while (getline(&buffer, &bufsize, stdin) != -1)
@@ -99,7 +165,7 @@ int main(int ac, char **av)
                         {
                                 if (strchr(args[0], '/') == 0)
                                 {
-                                        args_yazan(args, args[0]);
+                                        args_writer(args, args[0]);
                                 }
                                 if (execve(args[0], args, environ) == -1)
                                 {
