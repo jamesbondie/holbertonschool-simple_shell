16cc06fa3650fe0b0e2ed24930f4b22640574da5
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-11 21:57:09 +0000

    added cmd token

--- shell.c
+++ shell.c
@@ -60,94 +60,105 @@ void args_writer(char *arv[64], char *code_holder)
 }
 int main(int ac, char **av)
 {
-        pid_t my_pid;
-        size_t bufsize = 64;
-        int status, status_tutan = 5;
-        char *args[64];
-        char *buffer = malloc(bufsize * sizeof(char));
-        char *token;
-        int i = 0, j;
-        if (!buffer)
+    pid_t my_pid;
+    size_t bufsize = 64;
+    int status, status_tutan = 5;
+    char *args[64];
+    char *buffer = malloc(bufsize * sizeof(char));
+    char *token;
+    int i = 0, j;
+
+    if (!buffer)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    while (1)
+    {
+        if (getline(&buffer, &bufsize, stdin) == -1)
         {
-                perror("malloc");
-                exit(EXIT_FAILURE);
+            break;
         }
-
-        while (getline(&buffer, &bufsize, stdin) != -1 && ac > 0)
+        i = 0;
+        if (buffer[strlen(buffer) - 1] == '\n')
+            buffer[strlen(buffer) - 1] = '\0';
+        char *cmd = strdup(buffer);
+        char *cmd_token = strtok(cmd, "\n");
+        while (cmd_token != NULL)
         {
-                args[0] = NULL;
-                args[1] = NULL;
-                i = 0;
-                if (buffer[strlen(buffer) - 1] == '\n')
-                        buffer[strlen(buffer) - 1] = '\0';
-                token = strtok(buffer, " \n");
-                if (token == NULL)
-                {
-                        free(buffer);
-                        exit(EXIT_SUCCESS);
-                }
-                while (token != NULL)
-                {
-                        args[i] = strdup(token);
-                        if (!args[i])
-                        {
-                                perror("strdup");
-                                exit(EXIT_FAILURE);
-                        }
-                        token = strtok(NULL, " \n");
-                        i++;
-                }
-                args[i] = NULL;
-                if (strcmp(args[0], "exit") == 0)
-                {
-                        free(buffer);
-                        for (j = 0; j < i; j++)
-                                free(args[j]);
-                        if (status_tutan == 5)
-                                exit(0);
-                        else if (status_tutan == 512)
-                                exit(2);
-                }
-                if (strcmp(args[0], "env") == 0)
-                {        
+            token = strtok(cmd_token, " \n");
+            if (token == NULL)
+            {
+                break;
+            }
+            i = 0;
 
-                        free(buffer);
-                        for(j = 0; j < i; j++)
-                                free(args[j]);
-
-                        _printenv(environ);
-                        exit(EXIT_SUCCESS);
-
+            while (token != NULL)
+            {
+                args[i] = strdup(token);
+                if (!args[i])
+                {
+                    perror("strdup");
+                    exit(EXIT_FAILURE);
                 }
+                token = strtok(NULL, " \n");
+                i++;
+            }
+
+            args[i] = NULL;
+            if (strcmp(args[0], "exit") == 0)
+            {
+                free(buffer);
+                for (j = 0; j < i; j++)
+                    free(args[j]);
+                if (status_tutan == 5)
+                    exit(0);
+                else if (status_tutan == 512)
+                    exit(2);
+            }
+            if (strcmp(args[0], "env") == 0)
+            {
+                free(buffer);
+                for(j = 0; j < i; j++)
+                    free(args[j]);
+                _printenv(environ);
+            }
+            else
+            {
                 my_pid = fork();
                 if (my_pid == -1)
                 {
-                        perror("fork");
-                        exit(EXIT_FAILURE);
+                    perror("fork");
+                    exit(EXIT_FAILURE);
                 }
                 else if (my_pid == 0)
                 {
-                        if (strchr(args[0], '/') == 0)
-                        {
-                                args_writer(args, args[0]);
-                        }
-                        if (execve(args[0], args, environ) == -1)
-                        {
-                                fprintf(stderr, "%s: 1: %s: not found\n", av[0], buffer);
-                                free(buffer);
-                                for (j = 0; j < i; j++)
-                                free(args[j]);
-                                exit(127);
-                        }
+                    if (strchr(args[0], '/') == 0)
+                    {
+                        args_writer(args, args[0]);
+                    }
+                    if (execve(args[0], args, environ) == -1)
+                    {
+                        fprintf(stderr, "%s: 1: %s: not found\n", av[0], buffer);
+                        free(buffer);
+                        for (j = 0; j < i; j++)
+                            free(args[j]);
+                        exit(127);
+                    }
                 }
                 else
-                wait(&status);
-                status_tutan = status;
-                for (j = 0; j < i; j++)
-                free(args[j]);
+                {
+                    wait(&status);
+                    status_tutan = status;
+                    for (j = 0; j < i; j++)
+                        free(args[j]);
+                }
+            }
+            cmd_token = strtok(NULL, "\n");
         }
-        free(buffer);
-        if (status_tutan == 32512)
-                return(127);
-        return (0);
+    }
+    free(buffer);
+    if (status_tutan == 32512)
+        return 127;
+    return 0;
 }
