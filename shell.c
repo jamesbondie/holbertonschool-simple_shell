681d183060f778b1db19868920f309d73da9b753
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-12 08:55:55 +0000

    memory leak recvoered for ef else conditon of eof

--- shell.c
+++ shell.c
@@ -78,85 +78,83 @@ int main(int ac, char **av)
         {
                 nese = getline(&buffer, &bufsize, stdin);
                 if (nese == 1)
-                {
-                        free(buffer);
                         continue;
-                }
-                if (nese == -1)
-                {
+                else if (nese == -1)
                         break;
-                }
+                else
+                {
                         args[0] = NULL;
-                                args[1] = NULL;
-                                i = 0;
-                                if (buffer[strlen(buffer) - 1] == '\n')
-                                        buffer[strlen(buffer) - 1] = '\0';
-                                token = strtok(buffer, " \n\t");
-                                if (token == NULL)
+                        args[1] = NULL;
+                        i = 0;
+                        if (buffer[strlen(buffer) - 1] == '\n')
+                                buffer[strlen(buffer) - 1] = '\0';
+                        token = strtok(buffer, " \n\t");
+                        if (token == NULL)
+                        {
+                                free(buffer);
+                                exit(EXIT_SUCCESS);
+                        }
+                        while (token != NULL)
+                        {
+                                args[i] = strdup(token);
+                                if (!args[i])
                                 {
-                                        free(buffer);
-                                        exit(EXIT_SUCCESS);
-                                }
-                                while (token != NULL)
-                                {
-                                        args[i] = strdup(token);
-                                        if (!args[i])
-                                        {
-                                                perror("strdup");
-                                                exit(EXIT_FAILURE);
-                                        }
-                                        token = strtok(NULL, " \n\t");
-                                        i++;
-                                }
-                                args[i] = NULL;
-                                if (strcmp(args[0], "exit") == 0)
-                                {
-                                        free(buffer);
-                                        for (j = 0; j < i; j++)
-                                                free(args[j]);
-                                        if (status_tutan == 5)
-                                                exit(0);
-                                        else if (status_tutan == 512)
-                                                exit(2);
-                                }
-                                if (strcmp(args[0], "env") == 0)
-                                {        
-
-                                        free(buffer);
-                                        for(j = 0; j < i; j++)
-                                                free(args[j]);
-
-                                        _printenv(environ);
-                                        exit(EXIT_SUCCESS);
-
-                                }
-                                my_pid = fork();
-                                if (my_pid == -1)
-                                {
-                                        perror("fork");
+                                        perror("strdup");
                                         exit(EXIT_FAILURE);
                                 }
-                                else if (my_pid == 0)
-                                {
-                                        if (strchr(args[0], '/') == 0)
-                                        {
-                                                args_writer(args, args[0]);
-                                        }
-                                        if (execve(args[0], args, environ) == -1)
-                                        {
-                                                fprintf(stderr, "%s: 1: %s: not found\n", av[0], buffer);
-                                                free(buffer);
-                                                for (j = 0; j < i; j++)
-                                                free(args[j]);
-                                                exit(127);
-                                        }
-                                }
-                                else
-                                wait(&status);
-                                status_tutan = status;
+                                token = strtok(NULL, " \n\t");
+                                i++;
+                        }
+                        args[i] = NULL;
+                        if (strcmp(args[0], "exit") == 0)
+                        {
+                                free(buffer);
                                 for (j = 0; j < i; j++)
-                                free(args[j]);
+                                        free(args[j]);
+                                if (status_tutan == 5)
+                                        exit(0);
+                                else if (status_tutan == 512)
+                                        exit(2);
+                        }
+                        if (strcmp(args[0], "env") == 0)
+                        {        
+        
+                                free(buffer);
+                                for(j = 0; j < i; j++)
+                                        free(args[j]);
+        
+                                _printenv(environ);
+                                exit(EXIT_SUCCESS);
+        
+                        }
+                        my_pid = fork();
+                        if (my_pid == -1)
+                        {
+                                perror("fork");
+                                exit(EXIT_FAILURE);
+                        }
+                        else if (my_pid == 0)
+                        {
+                                if (strchr(args[0], '/') == 0)
+                                {
+                                        args_writer(args, args[0]);
+                                }
+                                if (execve(args[0], args, environ) == -1)
+                                {
+                                        fprintf(stderr, "%s: 1: %s: not found\n", av[0], buffer);
+                                        free(buffer);
+                                        for (j = 0; j < i; j++)
+                                        free(args[j]);
+                                        exit(127);
+                                }
+                        }
+                        else
+                        wait(&status);
+                        status_tutan = status;
+                        for (j = 0; j < i; j++)
+                        free(args[j]);
                 } 
+        }
         free(buffer);
         if (status_tutan == 32512)
                 return(127);
