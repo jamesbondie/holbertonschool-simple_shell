4b1a8d9c769d70e7c59daf811c95769500aa824b
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-12 07:32:40 +0000

    \n\t added for strtok

--- shell.c
+++ shell.c
@@ -95,7 +95,7 @@ int main(int ac, char **av)
                                 perror("strdup");
                                 exit(EXIT_FAILURE);
                         }
-                        token = strtok(NULL, " \n");
+                        token = strtok(NULL, " \t\n");
                         i++;
                 }
                 args[i] = NULL;
