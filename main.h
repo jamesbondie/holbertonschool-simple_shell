e788c5f18b4aa9cafe56f640c2446afd376eee87
jamesbondie <24772476-jamesbondie@users.noreply.replit.com>
2024-05-12 19:28:55 +0000

    unnecessary header files deleted

--- main.h
+++ main.h
@@ -7,15 +7,10 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-void remove_spaces(char *str);
+
 extern char **environ;
-void process_input(char *buffer, int ac, char *av[]);
-void handle_env(char *buffer, char *args[64], int ac, char *av[]);
-void handle_exit(char *buffer, char *args[64], int ac, char *av[]);
-char execute_command(char *args[64], char *av[]);
 int args_writer(char *arv[64], char *code_holder);
 void _printenv(char **envi);
 int custom_getenv(const char *name, char *args[64]);
-void cleanup_args(char *args[64]);
 
 #endif
